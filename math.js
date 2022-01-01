8134840589e69acf2dd2e6308455a6b2d0aabb43
Nilesh Mandal <nilesh@devcentrehouse.eu>
2022-01-01 17:02:50 +0000

    Create math.js

---
+++ math.js
@@ -0,0 +1,16 @@
+let _ = require('lodash')
+
+// console.log('console log ceil', _.ceil(3.6433 , 2));
+// Computes number rounded up to precision.
+
+// console.log('console log floor', _.floor(3.6433 , 2));
+// Computes number rounded down to precision.
+
+// console.log('console log max' ,  _.max([1,2,3,4,5,6]));
+// Computes the maximum value of array. If array is empty or falsey, undefined is returned.
+
+// console.log('console log mean', _.mean([1,2,3,4,5,6]));
+// Computes the mean of the values in array.
+
+// console.log('console log round', _.round(3.651,2));
+// Computes number rounded to precision.
